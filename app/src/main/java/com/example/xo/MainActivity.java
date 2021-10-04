3b35bfeb9687c39cb1c42efca280aeb2790991b5
DianaStaruseva <0832598@gmail.com>
2021-10-04 15:23:14 +0300

    init commit

---
+++ app/src/main/java/com/example/xo/MainActivity.java
@@ -0,0 +1,40 @@
+package com.example.xo;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+    Button btnReset, btn1, btn2, btn3, btn4, btn5, btn6, btn7, btn8, btn9;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        btn1.setOnClickListener(this::btnClick);
+
+        btnReset.setOnClickListener(view -> {
+            btn1.setText("");
+            btn2.setText("");
+            btn3.setText("");
+            btn4.setText("");
+            btn5.setText("");
+            btn6.setText("");
+            btn7.setText("");
+            btn8.setText("");
+            btn9.setText("");
+        });
+
+
+
+    }
+
+
+    public void btnClick(View v){
+
+    }
+
+}
